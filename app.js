0aba360a786e65368e1966a572309e134ac1ac9e
vahemm <vahemm@outlook.com>
2020-06-10 00:15:09 +0400

    Connecting MongoDB

--- app.js
+++ app.js
@@ -5,16 +5,22 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = config.get("port") || 5000;
 
-async function startDB (){
+async function startDB() {
     try {
-        await mongoose.connect(config.get("mongoURI"))
+        await mongoose.connect(config.get("mongoURI"),
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology:true,
+                useCreateIndex:true
+            });
+
+        app.listen(PORT, () => {
+            console.log(`Server run on port ${PORT}`)
+        })
     } catch (e) {
         console.log("DB error " + e.message);
         process.exit(1);
     }
 }
-startDB();
 
-app.listen(PORT, ()=>{
-    console.log(`Server run on port ${PORT}`)
-})
+startDB();
