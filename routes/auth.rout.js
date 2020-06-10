255e6adba55683468df0241b84b56d53d0f49ff8
vahemm <vahemm@outlook.com>
2020-06-11 01:23:58 +0400

    Connecting FrontEnd

--- routes/auth.rout.js
+++ routes/auth.rout.js
@@ -1,31 +1,78 @@
 const {Router} = require("express");
-const router = Router();
+const jwt = require("jsonwebtoken");
+const config = require("../config/default.json")
 const User = require("../Models/User");
 const bcrypt = require("bcrypt");
+const {check, validationResult} = require("express-validator");
 
-router.post("/registration", async (req, res) => {
-    try {
-        const {email, password} = req.body;
-        const condidat = User.findOne({email:email})
-        if (condidat){
-            return res.status(400).json({message:"email is repeated"})
+const router = Router();
+router.post(
+    "/registration",
+    [
+        check("email", "Is not email").isEmail(),
+        check("password", "Must be more than 8 simbol").isLength({min: 8})
+    ],
+    async (req, res) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.search(400).json({
+                    errors: errors.array(),
+                    message: "Uncorrently data"
+                })
+            }
+            const {email, password} = req.body;
+            const condidat = User.findOne({email: email})
+            if (condidat) {
+                return res.status(400).json({message: "email is repeated"})
+            }
+            const hashPassword = await bcrypt.hash(password, 12);
+            const user = new User({email, password: hashPassword});
+            await user.save();
+            res.status(201).json({message: "User has created"})
+
+        } catch (e) {
+            res.status(500).json({message: "Try again"})
         }
-        const hashPassword = await bcrypt.hash(password,12);
-        const user = new User({email, password:hashPassword});
-        res.status(201).json({message:"User has created"})
+    });
 
-    } catch (e) {
-        res.status(500).json({message: "Try again"})
-    }
-});
+router.post("/login",
+    [
+        check("email", "Is not email").normalizeEmail().isEmail(),
+        check("password", "write password").exists()
+    ],
+    async (req, res) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.search(400).json({
+                    errors: errors.array(),
+                    message: "Uncorrently data"
+                })
+            }
+            ;
 
-router.post("/login", async (req, res) => {
-    try {
+            const {email, password} = req.body;
+            const user = await User.findOne({email});
+            if (!user) {
+                return res.status(400).json({message: "User is not found"})
+            }
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (isMatch) {
+                return res.status(400).json({message: "False password"})
+            }
 
-    } catch (e) {
-        res.status(500).json({message: "Try again"})
-    }
-})
+            const token = jwt.sign(
+                {userID: user.id},
+                config.get("jwtSecret"),
+                {expiresIn: "1h"}
+            )
+            res.json({token, userID: user.id})
+
+        } catch (e) {
+            res.status(500).json({message: "Try again"})
+        }
+    });
 
 
 module.exports = router;
